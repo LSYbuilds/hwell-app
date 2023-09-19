e91d6ea686891a464cc5893be339e4c13d37ba42
DESKTOP-JUSC5N8\Administrator <builddav2009@gmail.com>
2023-09-19 11:45:17 +0900

    SEO 적용

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,23 +3,30 @@ import MapScene from '@/components/home/MapScene';
 import useInfo from '@/hooks/useInfo';
 import { Info } from '@/types/info';
 import { useEffect } from 'react';
+// SEO 적용하기
+import { NextSeo } from 'next-seo';
+
 interface IProps {
   infos: Info[];
 }
 export default function Home({ infos }: IProps) {
-  // 위경도에 대한 정보 및 naver의 map 객체를 전역으로 참조해서
-  // SWR 상태관리
+  // 위경도에 대한 정보(.json) 및 naver 의 map 객체를 전역으로 참조해서
+  // SWR 상태관리 ()
   const { initializeInfos } = useInfo();
 
-  // 최소 mount 가 되면 즉, html 이 출력이 되면
-  // props를 출력을 해본다.
+  // 최초 mount 가 되면 즉, html 이 출력이 되면
+  // props 를 출력을 해본다.
   useEffect(() => {
     console.log(infos);
     initializeInfos(infos);
-    // 새로운 정보가 들어온다면 다시 랜더링을 해주기 위해스
+    // 새로운 정보가 들어온다면 다시 랜더링 해주기 위해서
   }, [initializeInfos, infos]);
   return (
     <>
+      <NextSeo
+        title="건강검진센터 위치"
+        description="건강검진센터 위치서비스입니다."
+      />
       <Header />
       <MapScene />
     </>
@@ -32,7 +39,7 @@ export async function getStaticProps() {
   const infos = (await import('@/public/info.json')).default;
   return {
     props: { infos },
-    // 일정한 시간이 지나면 데이터를 다시자겨와서 pre-rendering 한디.
+    // 일정한 시간이 지나면 데이터를 다시가져와서 pre-rendering 한다.
     revalidate: 3600,
   };
 }
